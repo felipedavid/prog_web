6b9cbb87b862e2f68c259cc41d3a36969aa03572
Felipe David <Felipe David>
2022-07-22 07:50:03 -0300

    .

--- receita_3_next/pages/index.js
+++ receita_3_next/pages/index.js
@@ -1,5 +1,33 @@
-export default function Home() {
+export async function getServerSideProps(context) {
+  const res = await fetch(
+    `http://www.omdbapi.com/?i=tt3896198&apikey=2f9b03c7&s=${context.query.search}`
+  );
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default function Movies({ data }) {
   return (
-    <h1>Hello World!</h1>
-  )
+    <div>
+      <div>
+        <form method="GET">
+          <input name="search"></input>
+          <input type="submit"></input>
+        </form>
+        {data.Search.map((m) => (
+          <div>
+            {m.Title} - {m.Year}
+            <br />
+            <img src={m.Poster} />
+            <br />
+            <br />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
